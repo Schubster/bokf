9b6b06f2117eba842173c2a36e5de88ac6746441
Schubster <erwinhrnell@gmail.com>
2023-12-15 14:17:45 +0100

    kos

--- src/buttonTest.js
+++ src/buttonTest.js
@@ -1,29 +1,95 @@
 import React, { useState } from 'react';
 
 const InsertButton = () => {
+  const [transaktion, setTransaktion] = useState('');
+  const [konto, setKonto] = useState('');
+  const [summa, setSumma] = useState('');
   const [status, setStatus] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const choices = ['1930 Bankkonto', '2610 Utgående moms', '2640 ingående moms', '3040 fösäljning', '4010 Inköp'];
+  const kontoIdMap = {
+    '1930 Bankkonto': '0',
+    '2610 Utgående moms': 1,
+    '2640 ingående moms': 2,
+    '3040 fösäljning': 3,
+    '4010 Inköp': 4
+  };
 
   const handleInsertData = async () => {
     try {
-      const response = await fetch('http://localhost:3001/api/insertData', {
+      if (!transaktion || !konto || !summa) {
+        setStatus('Please fill in all fields');
+        return;
+      }
+  
+      setLoading(true);
+      console.log("konto:" + konto)
+      const kontoId = kontoIdMap[konto];
+      console.log("koasdokasd:" + kontoId);
+      const response = await fetch('http://localhost:3001/api/data/insertData', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ value1: 1, value2: 2, value3: 3 }), 
+        body: JSON.stringify({ 
+          transaktion_beskrivning: transaktion,
+          kontoRes: konto,
+          summa: summa 
+        }),
       });
-
-      const result = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const responseBody = await response.text();
+      console.log(responseBody);
+      const result = responseBody ? JSON.parse(responseBody) : null;
+  
       setStatus(`Data inserted: ${JSON.stringify(result)}`);
+      console.log(result);
     } catch (error) {
       console.error('Error inserting data:', error);
       setStatus('Error inserting data');
+    } finally {
+      setLoading(false);
     }
   };
+  
+  
 
   return (
     <div>
-      <button onClick={handleInsertData}>Insert Data</button>
+      <input
+        name='transaktion'
+        type='text'
+        value={transaktion}
+        onChange={(e) => setTransaktion(e.target.value)}
+      />
+{choices.map((choice, index) => (
+  <label key={index}>
+          {console.log()}
+
+    <input
+      type="radio"
+      name="konto"
+      value={kontoIdMap[choice]}
+      checked={konto === kontoIdMap[choice]}
+      onChange={(e) => setKonto(parseInt(e.target.value, 10))}  
+    />
+    {choice}
+  </label>
+))}
+
+      <input
+        name='summa'
+        type='number'
+        value={summa}
+        onChange={(e) => setSumma(e.target.value)}
+      />
+      <button onClick={handleInsertData} disabled={loading}>
+        {loading ? 'Inserting...' : 'Insert Data'}
+      </button>
       {status && <p>{status}</p>}
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -16,6 +16,25 @@ const pool = new Pool({
 app.use(express.json()); // Enable JSON parsing for incoming requests
 app.use(cors());
 
+
+const insertTransactionData = async (transaktion_beskrivning, kontoRes, summa) => {
+  try {
+    const result1 = await pool.query('INSERT INTO transaktion (beskrivning) VALUES ($1) RETURNING id', [transaktion_beskrivning]);
+    const v_transaktion_id = result1.rows[0].id;
+    console.log(kontoRes);
+    const result2 = await pool.query('INSERT INTO kontering (transaktion, konto, summa) VALUES ($1, $2, $3)', [v_transaktion_id, kontoRes, summa]);
+
+    console.log('Data inserted into the database');
+    return result2.rows[0];
+  } catch (error) {
+    console.error('Error executing query', error);
+    throw error;
+  }
+};
+
+
+
+// GET endpoint to retrieve data
 // GET endpoint to retrieve data
 app.get('/api/data', async (req, res) => {
   try {
@@ -30,25 +49,25 @@ app.get('/api/data', async (req, res) => {
 });
 
 app.get('/api/data/konton', async (req, res) => {
-    try {
-      console.log("kos omak");
-      const result = await pool.query('SELECT * FROM konton');
-      const data = result.rows;
+  try {
+    console.log("kos omak");
+    const result = await pool.query('SELECT * FROM konton');
+    const data = result.rows;
     console.log('Data retrieved from the database:', data);
     console.log('Query result:', result);
     res.json(data);
-    } catch (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Internal Server Error');
-    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
 })
 
 app.get('/api/data/transaktion', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM transaktion');
     const data = result.rows;
-  console.log('Data retrieved from the database:', data);
-  res.json(data);
+    console.log('Data retrieved from the database:', data);
+    res.json(data);
   } catch (error) {
     console.error('Error executing query', error);
     res.status(500).send('Internal Server Error');
@@ -56,16 +75,28 @@ app.get('/api/data/transaktion', async (req, res) => {
 })
 
 // POST endpoint to insert data
-app.post('/api/insertData', async (req, res) => {
+app.post('/api/data/insertData', async (req, res) => {
   try {
-    const { value1, value2, value3 } = req.body;
-    const insertQuery = 'INSERT INTO kontering (konto, summa, transaktion) VALUES ($1, $2, $3) RETURNING *';
-  
-    const result = await pool.query(insertQuery, [value1, value2, value3]);    
+    const { transaktion_beskrivning, kontoRes, summa } = req.body;
+    console.log(req)
 
-    const insertedData = result.rows[0];
+    const insertedData = await insertTransactionData(transaktion_beskrivning, kontoRes, summa);
+
     console.log('Data inserted into the database:', insertedData);
     res.json(insertedData);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// GET endpoint to retrieve data
+app.post('/api/data', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM kontering');
+    const data = result.rows;
+    console.log('Data retrieved from the database:', data);
+    res.json(data);
   } catch (error) {
     console.error('Error executing query', error);
     res.status(500).send('Internal Server Error');
